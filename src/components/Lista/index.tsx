939e52afb7ae1654d9b95015bf8bf490695c07ef
Rebecca Martins <61643695+mtrebecca@users.noreply.github.com>
2023-01-21 20:29:04 -0300

    Add files via upload
    Iniciando projeto

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import style from './Lista.module.scss';
+import Item from './Item';
+
+function Lista() {
+  const tarefas = [{
+    tarefa: 'React',
+    tempo: '02:00:00'
+  }, {
+    tarefa: 'Javascript',
+    tempo: '01:00:00'
+  }, {
+    tarefa: "Typescript",
+    tempo: "03:00:00"
+  }];
+  return (
+    <aside className={style.listaTarefas}>
+      <h2> Estudos do dia </h2>
+      <ul>
+        {tarefas.map((item, index) => (
+          <Item
+          key={index}
+           {...item}
+          />
+        ))}
+      </ul>
+    </aside>
+  )
+}
+
+export default Lista;
---
+++ src/pages/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Cronometro } from '../components/Cronometro';
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista';
+import style from './style.module.scss';
+
+function App() {
+  return (
+    <div className={style.AppStyle}>
+      <Formulario />
+      <Lista />
+      <Cronometro />
+    </div>
+  );
+}
+
+export default App;
